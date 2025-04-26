b6951c12bcf3c6a17bbd44a6535ed2ced9813719
David-ksantos <davidkmsantos@gmail.com>
2025-04-26 19:14:46 -0300

    Upando Projeto

---
+++ src/main/java/com/devl/Loja_api/LojaApiApplication.java
@@ -0,0 +1,16 @@
+package com.devl.Loja_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LojaApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LojaApiApplication.class, args);
+		System.out.println("Rod'ando...");
+	}
+
+}
